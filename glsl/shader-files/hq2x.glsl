39966d7e5dd6548b4e84f7ca5b0139bb9acaf3e4
Jules Blok <jules.blok@gmail.com>
2014-06-23 12:16:20 +0200

    Cosmetics.

--- glsl/shader-files/hq2x.glsl
+++ glsl/shader-files/hq2x.glsl
@@ -84,7 +84,7 @@ const mat3 yuv_matrix = mat3(0.299, 0.587, 0.114, -0.169, -0.331, 0.5, 0.5, -0.4
 const vec3 yuv_threshold = vec3(48.0, 7.0, 6.0);
 const mat3 yuv_weighted = mat3(yuv_threshold.x*yuv_matrix[0], yuv_threshold.y*yuv_matrix[1], yuv_threshold.z*yuv_matrix[2]);
 
-bool yuv_diff(vec3 yuv1, vec3 yuv2)
+bool diff(vec3 yuv1, vec3 yuv2)
 {
 	bvec3 res = greaterThan(abs(yuv1 - yuv2), vec3(10.0, 10.0, 10.0));
 	return res.x || res.y || res.z;
@@ -94,44 +94,32 @@ void main()
 {
 	vec2 fp = fract(vTexCoord[0].xy*TextureSize);
 	vec2 quad = sign(-0.5 + fp);
-
-	vec3 w1  = texture2D(Texture, vTexCoord[1].xw).rgb;
-	vec3 w2  = texture2D(Texture, vTexCoord[1].yw).rgb;
-	vec3 w3  = texture2D(Texture, vTexCoord[1].zw).rgb;
-
-	vec3 w4  = texture2D(Texture, vTexCoord[2].xw).rgb;
-	vec3 w5  = texture2D(Texture, vTexCoord[2].yw).rgb;
-	vec3 w6  = texture2D(Texture, vTexCoord[2].zw).rgb;
-
-	vec3 w7  = texture2D(Texture, vTexCoord[3].xw).rgb;
-	vec3 w8  = texture2D(Texture, vTexCoord[3].yw).rgb;
-	vec3 w9  = texture2D(Texture, vTexCoord[3].zw).rgb;
-
 	mat3 yuv = transpose(yuv_weighted);
 
-	vec3 yuv1 = yuv * w1;
-	vec3 yuv2 = yuv * w2;
-	vec3 yuv3 = yuv * w3;
+	vec3 w1  = yuv * texture2D(Texture, vTexCoord[1].xw).rgb;
+	vec3 w2  = yuv * texture2D(Texture, vTexCoord[1].yw).rgb;
+	vec3 w3  = yuv * texture2D(Texture, vTexCoord[1].zw).rgb;
 
-	vec3 yuv4 = yuv * w4;
-	vec3 yuv5 = yuv * w5;
-	vec3 yuv6 = yuv * w6;
+	vec3 w4  = yuv * texture2D(Texture, vTexCoord[2].xw).rgb;
+	vec3 w5  = yuv * texture2D(Texture, vTexCoord[2].yw).rgb;
+	vec3 w6  = yuv * texture2D(Texture, vTexCoord[2].zw).rgb;
 
-	vec3 yuv7 = yuv * w7;
-	vec3 yuv8 = yuv * w8;
-	vec3 yuv9 = yuv * w9;
+	vec3 w7  = yuv * texture2D(Texture, vTexCoord[3].xw).rgb;
+	vec3 w8  = yuv * texture2D(Texture, vTexCoord[3].yw).rgb;
+	vec3 w9  = yuv * texture2D(Texture, vTexCoord[3].zw).rgb;
 
 	bvec3 pattern[3];
-	pattern[0] = bvec3(yuv_diff(yuv5, yuv1), yuv_diff(yuv5, yuv2), yuv_diff(yuv5, yuv3));
-	pattern[1] = bvec3(yuv_diff(yuv5, yuv4), false               , yuv_diff(yuv5, yuv6));
-	pattern[2] = bvec3(yuv_diff(yuv5, yuv7), yuv_diff(yuv5, yuv8), yuv_diff(yuv5, yuv9));
-	bvec4 cross = bvec4(yuv_diff(yuv4, yuv2), yuv_diff(yuv2, yuv6), yuv_diff(yuv8, yuv4), yuv_diff(yuv6, yuv8));
+	pattern[0] =  bvec3(diff(w5, w1), diff(w5, w2), diff(w5, w3));
+	pattern[1] =  bvec3(diff(w5, w4), false       , diff(w5, w6));
+	pattern[2] =  bvec3(diff(w5, w7), diff(w5, w8), diff(w5, w9));
+	bvec4 cross = bvec4(diff(w4, w2), diff(w2, w6), diff(w8, w4), diff(w6, w8));
 
 	vec2 index;
 	index.x = dot(vec3(pattern[0]), vec3(1, 2, 4)) +
 			  dot(vec3(pattern[1]), vec3(8, 0, 16)) +
 			  dot(vec3(pattern[2]), vec3(32, 64, 128));
-	index.y = dot(vec4(cross), vec4(1, 2, 4, 8)) * (SCALE * SCALE) + dot(floor(fp * SCALE), vec2(1, SCALE));
+	index.y = dot(vec4(cross), vec4(1, 2, 4, 8)) * (SCALE * SCALE) +
+	          dot(floor(fp * SCALE), vec2(1, SCALE));
 
 	float dx = vTexCoord[0].w;
 	float dy = vTexCoord[0].z;
@@ -143,9 +131,9 @@ void main()
 
 	vec2 step = 1.0 / vec2(256.0, 16.0 * (SCALE * SCALE));
 	vec2 offset = step / 2.0;
-	vec4 vector = texture2D(LUT, index * step + offset);
-	float sum = dot(vector, vec4(1));
-	vec3 res = pixels * (vector / sum);
+	vec4 weights = texture2D(LUT, index * step + offset);
+	float sum = dot(weights, vec4(1));
+	vec3 res = pixels * (weights / sum);
 
 	gl_FragColor.rgb = res;
 }

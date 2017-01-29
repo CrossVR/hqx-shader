78d0682868dc690079e66805189b4123cc50302e
Jules Blok <jules.blok@gmail.com>
2017-01-29 13:56:05 +0100

    sample: Add support for program switching.

--- sample/main.cpp
+++ sample/main.cpp
@@ -12,9 +12,16 @@
 #include "lodepng.h"
 #include "linmath.h"
 
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <cassert>
+
+#ifdef _WIN32
+#define _ "\\"
+#else
+#define _ "/"
+#endif
 
 static const struct
 {
@@ -46,9 +53,21 @@ static const char* fragment_shader_text =
 "    gl_FragColor = texture2D(Texture, tex);\n"
 "}\n";
 
+static const char* shader_files[] = {
+    _"glsl" _"hq2x.glsl",
+    _"glsl" _"hq3x.glsl",
+    _"glsl" _"hq4x.glsl"
+};
+
+static const char* lut_files[] = {
+    _"resources" _"hq2x.png",
+    _"resources" _"hq3x.png",
+    _"resources" _"hq4x.png"
+};
+
 static const uint8_t indices[] = { 0, 1, 2, 0, 2, 3 };
 
-static BOOL should_scale = TRUE;
+static uint32_t image_width, image_height, image_scale = 2;
 
 static void error_callback(int error, const char* description)
 {
@@ -61,8 +80,13 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 
-    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
-        should_scale = !should_scale;
+	if (GLFW_KEY_1 <= key && key <= GLFW_KEY_4 && action == GLFW_PRESS)
+	{
+		image_scale = key - GLFW_KEY_0;
+
+        if (mods != GLFW_MOD_SHIFT)
+		    glfwSetWindowSize(window, image_width * image_scale, image_height * image_scale);
+	}
 }
 
 static void read_file(const char* filename, std::vector<char>& buffer)
@@ -81,7 +105,7 @@ static void read_file(const char* filename, std::vector<char>& buffer)
     file.read(buffer.data(), size);
 }
 
-static GLuint load_texture(GLenum stage, uint32_t* width, uint32_t* height, const char* filename)
+static GLuint load_texture(uint32_t* width, uint32_t* height, const char* filename)
 {
     std::vector<uint8_t> image;
     uint32_t w, h, error;
@@ -95,7 +119,7 @@ static GLuint load_texture(GLenum stage, uint32_t* width, uint32_t* height, cons
     }
 
     glGenTextures(1, &texture);
-    glActiveTexture(stage);
+    glActiveTexture(GL_TEXTURE9); // loading stage
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -168,23 +192,23 @@ static GLuint link_program(GLuint vertex_shader, GLuint fragment_shader)
 int main(int argc, const char* argv[])
 {
     GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program, texture, lut, passthrough;
-    GLint mvp_location, samp_location, lut_location, vpos_location, vtex_location, tsize_location;
-    uint32_t width, height, scale;
-    std::vector<char> shader;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, texture;
+    std::vector<GLuint> programs, lut_textures;
+    GLint vpos_location, vtex_location;
     mat4x4 mvp;
 
-    if (argc < 4)
+    if (argc < 2)
     {
-        printf("usage %s: scale shader lut image\n", argv[0]);
+        printf("usage %s: <hqx-shader folder> [image file]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    scale = atoi(argv[1]);
-    if (scale <= 0)
-        exit(EXIT_FAILURE);
-
-    read_file(argv[2], shader);
+    std::string base_path = argv[1];
+    std::string image_path(base_path);
+    if (argc > 2)
+        image_path = argv[2];
+    else
+        image_path.append(_"sample" _"pixelart0.png");
 
     glfwSetErrorCallback(error_callback);
 
@@ -207,57 +231,74 @@ int main(int argc, const char* argv[])
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     glfwSwapInterval(1);
 
-    texture = load_texture(GL_TEXTURE0, &width, &height, argv[4]);
-    lut = load_texture(GL_TEXTURE1, NULL, NULL, argv[3]);
-    glfwSetWindowSize(window, width * scale, height * scale);
+    texture = load_texture(&image_width, &image_height, image_path.c_str());
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    programs.push_back(NULL);
+    lut_textures.push_back(NULL);
     vertex_shader = compile_shader(GL_VERTEX_SHADER, vertex_shader_text);
     fragment_shader = compile_shader(GL_FRAGMENT_SHADER, fragment_shader_text);
-    passthrough = link_program(vertex_shader, fragment_shader);
-    glUseProgram(passthrough);
-    glUniform1i(glGetUniformLocation(passthrough, "Texture"), 0);
+    programs.push_back(link_program(vertex_shader, fragment_shader));
+    lut_textures.push_back(NULL);
+    glUniform1i(glGetUniformLocation(programs[1], "Texture"), 0);
 
-    vertex_shader = compile_shader(GL_VERTEX_SHADER, shader.data());
-    fragment_shader = compile_shader(GL_FRAGMENT_SHADER, shader.data());
-    program = link_program(vertex_shader, fragment_shader);
-    glUseProgram(program);
-
-    mvp_location = glGetUniformLocation(program, "MVPMatrix");
-    samp_location = glGetUniformLocation(program, "Texture");
-    lut_location = glGetUniformLocation(program, "LUT");
-    tsize_location = glGetUniformLocation(program, "TextureSize");
-    vpos_location = glGetAttribLocation(program, "VertexCoord");
-    vtex_location = glGetAttribLocation(program, "TexCoord");
-
-    mat4x4_identity(mvp);
-    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
-    glUniform1i(samp_location, 0);
-    glUniform1i(lut_location, 1);
-    glUniform2f(tsize_location, (float)width, (float)height);
-
+    vpos_location = glGetAttribLocation(programs[1], "VertexCoord");
+    vtex_location = glGetAttribLocation(programs[1], "TexCoord");
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location, 4, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) 0);
+        sizeof(vertices[0]), (void*)0);
     glEnableVertexAttribArray(vtex_location);
     glVertexAttribPointer(vtex_location, 4, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) (sizeof(float) * 4));
+        sizeof(vertices[0]), (void*)(sizeof(float) * 4));
 
+    mat4x4_identity(mvp);
+    for (int i = 0; i < 3; i++)
+    {
+        std::vector<char> shader;
+        std::string shader_path(base_path);
+        shader_path.append(shader_files[i]);
+
+        read_file(shader_path.c_str(), shader);
+        vertex_shader = compile_shader(GL_VERTEX_SHADER, shader.data());
+        fragment_shader = compile_shader(GL_FRAGMENT_SHADER, shader.data());
+        GLuint program = link_program(vertex_shader, fragment_shader);
+
+        GLint mvp_location = glGetUniformLocation(program, "MVPMatrix");
+        GLint samp_location = glGetUniformLocation(program, "Texture");
+        GLint lut_location = glGetUniformLocation(program, "LUT");
+        GLint tsize_location = glGetUniformLocation(program, "TextureSize");
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+        glUniform1i(samp_location, 0);
+        glUniform1i(lut_location, 1);
+        glUniform2f(tsize_location, (float)image_width, (float)image_height);
+
+        std::string lut_path(base_path);
+        lut_path.append(lut_files[i]);
+        GLuint lut = load_texture(nullptr, nullptr, lut_path.c_str());
+
+        programs.push_back(program);
+        lut_textures.push_back(lut);
+    }
+
+    glfwSetWindowSize(window, image_width * image_scale, image_height * image_scale);
     while (!glfwWindowShouldClose(window))
     {
-        int fwidth, fheight;
-        glfwGetFramebufferSize(window, &fwidth, &fheight);
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
 
-        glViewport(0, 0, fwidth, fheight);
+        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        if (should_scale)
-            glUseProgram(program);
-        else
-            glUseProgram(passthrough);
+        glUseProgram(programs[image_scale]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, lut_textures[image_scale]);
 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
 
